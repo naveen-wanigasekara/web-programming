f62430a6e8e51b0b25350ca419d306a36c3cfce0
naveen-wanigasekara <naveenwanigasekara@outlook.com>
2023-07-01 13:53:09 +0530

    js basics update

---
+++ JS-basics/index_notation.js
@@ -0,0 +1,11 @@
+let person = {
+    name : 'Naveen',
+    age : 26
+};
+// Dot Notation
+person.name = 'Wanigasekara';
+
+// Bracket Notation
+person['name'] = 'Cedric';
+
+console.log(person.name)
---
+++ JS-basics/index_typesoffunctions.js
@@ -0,0 +1,12 @@
+
+// Performing a task
+function greet(name, lastName){
+    console.log('Hello ' + name + ' ' + lastName)
+}
+
+// Calculating a valu
+function squar(number){
+    return number * number;
+}
+
+console.log(squar(2));
