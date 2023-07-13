0ab5f2795fbb02c29203b2bacedf46c270154134
naveen-wanigasekara <naveenwanigasekara@outlook.com>
2023-07-13 18:20:57 +0530

    greeting updated

--- JS/hello.js
+++ JS/hello.js
@@ -1,6 +1,43 @@
-document.addEventListener('DOMContentLoaded', function(){    
-    document.querySelector('form').onsubmit = function(){
-        const name = document.querySelector('#name').value;
-        alert(`Hello, ${name}!`); 
+document.addEventListener('DOMContentLoaded', function(){
+// Retrieve name from localStorage if it exists    
+    let name = localStorage.getItem('name');
+    if (name) {
+        name = document.querySelector('#name').value;
+        document.querySelector('h1').innerHTML = `Hello ${name} !`;
+    }
+    
+    document.querySelector('form').onsubmit = function(event){
+        event.preventDefault();
+
+ // Get the entered name
+        let name = document.querySelector('#name').value;
+
+// Store the name in localStorage
+        localStorage.setItem('name', name);
+
+// Update the greeting message
+        document.querySelector('h1').innerHTML = `Hello ${name} !`;
+
+// Clear the input field
+        document.querySelector('#name').value = ' ';
+
+// Call setGreeting() to update the time-based greeting
+        setGreeting(name);
+         
     };
 });
+
+function setGreeting() {
+    let time = new Date ();
+    let hour = time.getHours();
+    let greeting;
+
+    if (hour < 12 ) {
+        greeting = `Good Morning!`;
+    } else if (hour < 18) {
+        greeting = `Good Afternoon!`;
+    } else {
+        greeting = `Good Evening!`;
+    }
+    document.querySelector('h2').innerHTML = greeting;
+};
