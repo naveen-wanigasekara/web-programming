306f38142046339e008558ebf12815fe24273d27
naveen-wanigasekara <naveenwanigasekara@outlook.com>
2023-07-13 16:22:32 +0530

    updated

--- JS/counter.js
+++ JS/counter.js
@@ -3,14 +3,20 @@ if (!localStorage.getItem('counter')){
     localStorage.setItem('counter', 0);
 }
 
-function count(){
+function countUp(){
     let counter = localStorage.getItem('counter');
     counter++;
     document.querySelector('h1').innerHTML = counter;
     localStorage.setItem('counter', counter)
 }
-
+function countDown(){
+    let counter = localStorage.getItem('counter');
+    counter--;
+    document.querySelector('h1').innerHTML = counter;
+    localStorage.setItem('counter', counter)
+}
 document.addEventListener('DOMContentLoaded', function(){
     document.querySelector('h1').innerHTML = localStorage.getItem('counter');
-    document.querySelector('button').onclick = count;
+    document.querySelector('#increase').onclick = countUp;
+    document.querySelector('#decrease').onclick = countDown;
 });
